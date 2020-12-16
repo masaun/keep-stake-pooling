e3f2fd9d0bcc223f34f2e32e5ff6455c7ce2adeb
masaun <masanoriuno@gmail.com>
2020-12-16 21:49:12 +0900

    Declare minimum Keep stake amount with 70,000 KEEP

--- contracts/KeepStakePooling.sol
+++ contracts/KeepStakePooling.sol
@@ -6,11 +6,16 @@ import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import { KeepToken } from "./keep-core/KeepToken.sol";
 
 
+/***
+ * @notice - This is a smart contract to allow smaller Keep owners to delegate a stake lower than the current minimum Keep stake
+ * @notice - Minimum Keep stake amount is 70,000 KEEP.
+ **/
 contract KeepStakePooling {
 
     KeepToken public keepToken;
 
     address KEEP_TOKEN; 
+    uint MINIMUM_STAKE_KEEP_AMOUNT = 70000;  /// [Note]: Minimum Keep stake amount is 70,000 KEEP
 
     constructor (KeepToken _keepToken) public {
         keepToken = _keepToken;
