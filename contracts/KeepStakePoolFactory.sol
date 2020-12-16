9ed2dcc7c4ccac1ae0bee068fba7a647d742023a
masaun <masanoriuno@gmail.com>
2020-12-16 22:06:24 +0900

    Define createKeepStakePool() and Declare 'currentKeepStakePoolId'

--- contracts/KeepStakePoolFactory.sol
+++ contracts/KeepStakePoolFactory.sol
@@ -4,21 +4,32 @@ import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 import { KeepToken } from "./keep-core/KeepToken.sol";
+import { KeepStakePool } from "./KeepStakePool.sol";
 
 
 /***
  * @notice - This is a smart contract to create a new Keep Stake Pool
  **/
 contract KeepStakePoolFactory {
+    uint currentKeepStakePoolId;  /// Current KeepStakePool ID
+    address KEEP_TOKEN; 
 
     KeepToken public keepToken;
-
-    address KEEP_TOKEN; 
 
     constructor (KeepToken _keepToken) public {
         keepToken = _keepToken;
 
         KEEP_TOKEN = address(_keepToken);
     }
+
+
+    /***
+     * @notice - Create a new KeepStakePool
+     **/
+    function createKeepStakePool(KeepToken _keepToken) public returns (address _keepStakePool) {
+        KeepStakePool keepStakePool = new KeepStakePool(_keepToken);
+        return address(keepStakePool);
+    }
+
 
 }
